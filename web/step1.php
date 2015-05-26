317db24ce3e91c4f7af19f2d62e82f2a15b8a429
Randi Lee Harper <randi@onlineabuseprevention.org>
2015-05-26 05:50:45 -0700

    add step1.php - html form
    i can't frontend.

---
+++ web/step1.php
@@ -0,0 +1,82 @@
+<?php
+// basically a garbage file - some logic is good to use, but this was pulled from a diff project.
+require('conf.php');
+require('lib.php');
+
+session_start();
+
+// User is not logged in, redirect
+if(empty($_SESSION['oauth_uid'])){
+	header('Location: twitter_login.php');
+	exit;
+}
+
+?>
+
+<html>
+<head>
+<title>OAPI: Shields Up</title>
+
+<style>
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 1.2em;
+}
+
+table {
+	margin: 0px;
+	border: 0px;
+	padding: 10px;
+}
+</style>
+</head>
+
+<body>
+
+<?php
+$sock = stream_socket_client("unix://$socket_file", $errno, $errstr);
+
+// todo: make this error message prettier?
+
+if ($errno) {
+	print("something is wrong: <strong>$errstr!</strong><br />\n");
+	print("the backend server probably isn't running. kick it.\n");
+	print("</body></html>");
+	exit;
+}
+
+// create a connection to our backend server
+// serves no purpose beyond ensuring our auth works
+$ret = login_to_backend(	$sock,
+				$app_key,
+				$app_secret,
+				$_SESSION['oauth_token'],
+				$_SESSION['oauth_token_secret'] );
+
+// todo: put in a link to the webpage. we should add that to the conf file.
+if ( $ret == "AUTH_ERR" ) {
+	print("There seems to be a problem logging you in to twitter. ");
+	print("Try hitting back in your browser to restart the process.\n");
+	print("</body></html>");
+	exit;
+}
+
+?>
+<form action="step2.php" method="POST">
+
+<table>
+<tr>
+<td>Twitter username:</td>
+<td><input type="text" name="username"></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="Submit"></td>
+</tr>
+</table>
+
+</form>
+
+</body>
+</html>
+
