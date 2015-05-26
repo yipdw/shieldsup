694c2717d0f3b11880eda041afe82220772008c7
Randi Lee Harper <randi@onlineabuseprevention.org>
2015-05-26 06:50:50 -0700

    only for debugging.

--- web/step2.php
+++ web/step2.php
@@ -11,65 +11,15 @@ if(empty($_SESSION['oauth_uid'])){
 	exit;
 }
 
-?>
-
-<html>
-<head>
-<title>OAPI: Shields Up</title>
-<style>
-body {
-	font-family: Arial, Helvetica, sans-serif;
-	font-size: 1.2em;
-}
-
-table {
-	margin: 100px;
-}
-
-table#t_list {
-	margin: 0px;
-	border: 0px;
-	padding: 0px;
-	width: 90%;
-}
-
-table#t_list td {
-	border: 0px;
-	padding: 4px;
-	text-align: left;
-	font-size: 0.8em;
-	// font-family: "Lucida Console", Monaco, monospace;
-}
-
-table#t_list tr:nth-child(even) {
-	background-color: #eee;
-}
-
-table#t_list tr:nth-child(odd) {
-	background-color: #fff;
-}
-
-table, td {
-	font-family: "Arial Black", Helvetica, sans-serif;
-	border: 1px solid black;
-	border-collapse: collapse;
-}
-td {
-	padding: 8px;
-	text-align: center;
-}
-</style>
-</head>
-<body>
-
-<?php
 $sock = stream_socket_client("unix://$socket_file", $errno, $errstr);
 
 // todo: make this error message prettier?
 
 if ($errno) {
+	print("<html><body>\n");
 	print("something is wrong: <strong>$errstr!</strong><br />\n");
 	print("the backend server probably isn't running. kick it.\n");
+	print("</body></html>\n");
 	exit;
 }
 
@@ -82,18 +32,27 @@ $ret = login_to_backend(	$sock,
 
 // todo: put in a link to the webpage. we should add that to the conf file.
 if ( $ret == "AUTH_ERR" ) {
+	print("<html><body>\n");
 	print("There seems to be a problem logging you in to twitter. ");
 	print("Try hitting back in your browser to restart the process.\n");
-	print("</body></html>");
+	print("</body></html>\n");
 	exit;
 }
 
-// pretty much everything beneath this line needs to be changed.
-// put in some intro comments about how things will go down.
-?>
-<table><tr>
-<td>blocked</td>
-<?php
+// figure out how to get the html form stuff... oh god. how. it's post. i don't know.
+// this is for debug and not legitimate.
+
+print("<html><body>");
+
+print("<b>env: </b>\n");
+print_r($_ENV);
+print("\n<p></p>\n";
+print("<b>post: </b>\n");
+print_r($_POST);
+print("\n</body></html>\n");
+
+exit;
+
 // alllll of this logic needs to be changed to fit what shields up actually does.
 // change protocol, change output.
 fwrite($sock, 'blocked?'."\n");
