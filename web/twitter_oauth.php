e8ee027645ce8809865192eb8d45072b2efc6157
Randi Lee Harper <randi@onlineabuseprevention.org>
2015-05-25 20:14:32 -0700

    updated sql queries
    reflective of new db design.
    still need to sanitize input to prevent sql injection attacks.
    should probably do some error checking, too.

--- web/twitter_oauth.php
+++ web/twitter_oauth.php
@@ -9,6 +9,10 @@ require "twitteroauth/twitteroauth.php";
 
 session_start();
 
+// THIS IS THE UGLIEST CODE EVER BUT SOMEONE ELSE WROTE IT SO I DON'T FEEL BAD.
+// regardless, we should still fix it.
+// seriously i can barely read this crap.
+
 if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty($_SESSION['oauth_token_secret'])){
 	// We've got everything we need
 	// TwitterOAuth instance, with two new parameters we got in twitter_login.php
@@ -25,21 +29,22 @@ if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty
 		header('Location: twitter_login.php');
 	} else {
 		// Let's find the user by its ID
-		$query = mysql_query("SELECT * FROM oauth WHERE oauth_provider = 'twitter' AND oauth_uid = ". $user_info->id);
+		// todo: error checking?! sanitize.
+		$query = mysql_query("SELECT * FROM tokens WHERE oauth_uid = ". $user_info->id);
 		$result = mysql_fetch_array($query);
  
 		// If not, let's add it to the database
 		if(empty($result)){
-			$query = mysql_query("INSERT INTO oauth (oauth_provider, oauth_uid, username, oauth_token, oauth_secret) VALUES ('twitter', {$user_info->id}, '{$user_info->screen_name}', '{$access_token['oauth_token']}', '{$access_token['oauth_token_secret']}')");
-			$query = mysql_query("SELECT * FROM oauth WHERE id = " . mysql_insert_id());
+			// todo: this needs to be sanitized.
+			$query = mysql_query("INSERT INTO tokens (userid, oauth_token, oauth_secret, added, accessed) VALUES ('{$user_info->id}', '{$access_token['oauth_token']}', '{$access_token['oauth_token_secret']}', NOW(), NOW())");
+			$query = mysql_query("SELECT * FROM tokens WHERE id = " . mysql_insert_id());
 			$result = mysql_fetch_array($query);
 		} else {
 			// Update the tokens
-			$query = mysql_query("UPDATE oauth SET oauth_token = '{$access_token['oauth_token']}', oauth_secret = '{$access_token['oauth_token_secret']}' WHERE oauth_provider = 'twitter' AND oauth_uid = {$user_info->id}");
+			$query = mysql_query("UPDATE tokens SET oauth_token = '{$access_token['oauth_token']}', oauth_secret = '{$access_token['oauth_token_secret']}', accessed = NOW() WHERE oauth_uid = {$user_info->id}");
 		}
  
 		$_SESSION['id'] = $result['id'];
-		$_SESSION['username'] = $result['username'];
 		$_SESSION['oauth_uid'] = $result['oauth_uid'];
 		$_SESSION['oauth_provider'] = $result['oauth_provider'];
 		$_SESSION['oauth_token'] = $result['oauth_token'];
