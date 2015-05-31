eca414817f310e4b6f1e64e54ac5048ce414d7cf
Blake Swopes <blake.swopes@reachlocal.com>
2015-05-31 14:58:05 -0700

    Moved common code from step1-4 to lib.php. Created two new functions.
    su_session_start() - Call session_start(), verify oath uid not empty, open socket and log in to backend.
    print_html_header() - Standard html header from step1.php
    
    Missing html header on socket error.

--- web/step4.php
+++ web/step4.php
@@ -2,42 +2,7 @@
 require('conf.php');
 require('lib.php');
 
-session_start();
-
-// User is not logged in, redirect
-if(empty($_SESSION['oauth_uid'])){
-	header('Location: twitter_login.php');
-	exit;
-}
-
-$sock = stream_socket_client("unix://$socket_file", $errno, $errstr);
-
-// todo: make this error message prettier?
-
-if ($errno) {
-	print("<html><body>\n");
-	print("something is wrong: <strong>$errstr!</strong><br />\n");
-	print("the backend server probably isn't running. kick it.\n");
-	print("</body></html>");
-	exit;
-}
-
-// create a connection to our backend server
-// serves no purpose beyond ensuring our auth works
-$ret = login_to_backend(	$sock,
-				$app_key,
-				$app_secret,
-				$_SESSION['oauth_token'],
-				$_SESSION['oauth_token_secret'] );
-
-// todo: put in a link to the webpage. we should add that to the conf file.
-if ( $ret == "AUTH_ERR" ) {
-	print("<html><body>\n");
-	print("There seems to be a problem logging you in to twitter. ");
-	print("Try hitting back in your browser to restart the process.\n");
-	print("</body></html>");
-	exit;
-}
+    su_session_start();
 
 // i have literally no idea why i'm writing newlines like this.
 fwrite($sock, 'GET_LIST'."\n");
