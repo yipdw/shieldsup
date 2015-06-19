0a62d831823c36683631e02d0bdcbf951e44497f
Blake Swopes <blake.swopes@reachlocal.com>
2015-06-19 08:35:03 -0700

    Just whitespaces for readability.

--- web/twitter_oauth.php
+++ web/twitter_oauth.php
@@ -14,20 +14,25 @@ session_start();
 // seriously i can barely read this crap.
 
 if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty($_SESSION['oauth_token_secret'])){
-	// We've got everything we need
+
+    // We've got everything we need
 	// TwitterOAuth instance, with two new parameters we got in twitter_login.php
 	$twitteroauth = new TwitterOAuth($app_key, $app_secret, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-	// Let's request the access token
+
+    // Let's request the access token
 	$access_token = $twitteroauth->getAccessToken($_GET['oauth_verifier']);
-	// Save it in a session var
+
+    // Save it in a session var
 	$_SESSION['access_token'] = $access_token;
-	// Let's get the user's info
+
+    // Let's get the user's info
 	$user_info = $twitteroauth->get('account/verify_credentials');
 
 	if(isset($user_info->error)){
 		// Something's wrong, go back to square 1
 		header('Location: twitter_login.php');
-	} else {
+
+    } else {
 		// Let's find the user by its ID
 		// todo: error checking?! sanitize.
 		$query = mysql_query("SELECT * FROM tokens WHERE userid = ". $user_info->id);
@@ -39,7 +44,8 @@ if(!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty
 			$query = mysql_query("INSERT INTO tokens (userid, oauth_token, oauth_secret, added) VALUES ('{$user_info->id}', '{$access_token['oauth_token']}', '{$access_token['oauth_token_secret']}', NOW())");
 			$query = mysql_query("SELECT * FROM tokens WHERE id = " . mysql_insert_id());
 			$result = mysql_fetch_array($query);
-		} else {
+
+        } else {
 			// Update the tokens
 			$query = mysql_query("UPDATE tokens SET oauth_token = '{$access_token['oauth_token']}', oauth_secret = '{$access_token['oauth_token_secret']}', accessed = NOW() WHERE userid = {$user_info->id}");
 		}
